03aad6311ae85889657f15395dad6254dfec30c1
Rupert Broad <rupert.broad@gmail.com>
2018-02-24 15:06:38 +0100

    add outputs (#3)
    * add outputs
    
    * tfm fmt
    
    * fix output names

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "vpc_id" {
+  value = "${module.network.vpc_id}"
+}
 
+output "private_subnet_ids" {
+  value = "${module.network.private_subnet_ids}"
+}
+
+output "public_subnet_ids" {
+  value = "${module.network.public_subnet_ids}"
+}
+
+output "private_subnets_cidrs" {
+  value = "${module.network.private_subnet_cidrs}"
+}
+
+output "public_subnets_cidrs" {
+  value = "${module.network.public_subnet_cidrs}"
+}
